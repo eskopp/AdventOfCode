982923afe4ac56f21ea35f02eb15e69be8d15bd6
eskopp <skopp.erik@gmail.com>
2023-12-14 13:41:01 +0100

    Header

--- 2023/Day01/AoC_2023_01_1.py
+++ 2023/Day01/AoC_2023_01_1.py
@@ -1,10 +1,12 @@
 from colorama import Fore, init
+import os
 
-
+# Header
 init(autoreset=True)
-print(f"\n{Fore.GREEN}Advent of Code 2023, Day 1, Task 1")
-print(f"Task:\t\t\t\t {Fore.BLUE} https://adventofcode.com/2023/day/1")
-print(f"Load input:\t\t\t {Fore.BLUE} input.txt")
+print(f"{Fore.GREEN}Advent of Code 2023, Day 1, Task 1")
+print(f"Task: \t {Fore.BLUE} https://adventofcode.com/2023/day/1")
+print(f"Folder:  {Fore.BLUE} {os.path.abspath('.')}")
+print(f"Input: \t {Fore.BLUE} input.txt")
 
 
 def calculate_calibration_sum(calibration_document):
@@ -24,4 +26,4 @@ with open("./input.txt", "r") as file:
 
 result = calculate_calibration_sum(calibration_document)
 
-print(f"Value:\t\t\t\t  {Fore.BLUE}{result}")
+print(f"Value:\t  {Fore.BLUE}{result}")
---
+++ 2023/Day02/AoC_2023_02_1.py
@@ -0,0 +1,11 @@
+from colorama import Fore, init
+import os
+
+# Header
+init(autoreset=True)
+print(f"{Fore.GREEN}Advent of Code 2023, Day 2, Task 1")
+print(f"Task: \t {Fore.BLUE} https://adventofcode.com/2023/day/2")
+print(f"Folder:  {Fore.BLUE} {os.path.abspath('.')}")
+print(f"Input: \t {Fore.BLUE} input.txt")
+
+# Solution
