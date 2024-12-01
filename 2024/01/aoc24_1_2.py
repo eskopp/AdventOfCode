6aca395e27de1b1574332663bd8fac91c6589db3
eskopp <skopp.erik@gmail.com>
2024-12-01 20:16:45 +0100

    asd

--- 2024/01/aoc24_1_2.py
+++ 2024/01/aoc24_1_2.py
@@ -1,47 +1,22 @@
-# AoC 2024 - Day 1 - Part 2
 # https://erik-skopp.de/AdventofCode/2024/1/
 #
-# Day 1 - Historian Hysteria
+# Day 1 - Part 1 - Historian Hysteria
+# result: 1722302
+
 import os
 
-# Die Pfade können je nach Ort abweichen
+# Arbeitsverzeichnis setzen
 os.chdir(os.path.join("2024", "01"))
 
-# unsortiere Listen
-left = []
-right = []
-liste = []
+# Initialisierung der Variablen
+result = 0
 
-# Trennen der Daten in die verschiedenen Spalten
+# Daten direkt verarbeiten
 with open("input.in", "r") as file:
     for line in file:
-        line = line.strip().split("   ")
-        left.append(line[0])
-        right.append(line[1])
+        left, right = map(int, line.strip().split("   "))
+        result += abs(left - right)
 
-
-# Sortieren der Listen
-left_sort = sorted(left)
-right_sort = sorted(right)
-
-# Löschen von Altlasten
-del left
-del right
-
-# Berechnen der Paarungen
-index = []
-for runner in range(len(left_sort)):
-    value = int(left_sort[runner]) - int(right_sort[runner])
-    # Betragsfunktion
-    if value < 0:
-        value *= -1
-    index.append(value)
-    del value
-
-# Ermittle das Ergebniss
-result = 0
-for runner in index:
-    result += runner
-
-# Test Ausgabe
+# Ausgabe des Ergebnisses
 print(result)
+del result
--- docs/content/en/2024/1/solution_python.md
+++ docs/content/en/2024/1/solution_python.md
@@ -5,7 +5,38 @@ type: docs
 ---
 
 
+# Download
 {{< cards >}}
 {{< card title="Example" link="https://github.com/eskopp/AdventOfCode/blob/main/2024/01/example.in" icon="user" tag="Github">}}
 {{< card title="Input" link="https://github.com/eskopp/AdventOfCode/blob/main/2024/01/input.in" icon="user" tag="Github">}}
 {{< /cards >}}
+
+
+
+
+## Part 1
+```python
+# https://erik-skopp.de/AdventofCode/2024/1/
+#
+# Day 1 - Part 1 - Historian Hysteria
+# result: 1722302
+
+import os
+
+# Arbeitsverzeichnis setzen
+os.chdir(os.path.join("2024", "01"))
+
+# Initialisierung der Variablen
+result = 0
+
+# Daten direkt verarbeiten
+with open("input.in", "r") as file:
+    for line in file:
+        left, right = map(int, line.strip().split("   "))
+        result += abs(left - right)
+
+# Ausgabe des Ergebnisses
+print(result)
+del result
+```
+See: [https://github.com/eskopp/AdventOfCode/blob/main/2024/01/aoc24_1_1.py](https://github.com/eskopp/AdventOfCode/blob/main/2024/01/aoc24_1_1.py)
