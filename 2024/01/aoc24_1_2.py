0e92d33495f18226535bdbc42e61c0822b1a5e97
eskopp <skopp.erik@gmail.com>
2024-12-01 21:10:56 +0100

    Update aoc24_1_2.py

--- 2024/01/aoc24_1_2.py
+++ 2024/01/aoc24_1_2.py
@@ -7,44 +7,23 @@
 
 import os
 
-# Die Pfade können je nach Ort abweichen
+# Arbeitsverzeichnis setzen
 os.chdir(os.path.join("2024", "01"))
 
-# unsortiere Listen
+# Listen für linke und rechte Werte
 left = []
 right = []
-liste = []
 
-# Trennen der Daten in die verschiedenen Spalten
+# Datei einlesen und Daten in Spalten aufteilen
 with open("input.in", "r") as file:
     for line in file:
         line = line.strip().split("   ")
-        left.append(line[0])
-        right.append(line[1])
+        if len(line) == 2:
+            left.append(line[0])
+            right.append(line[1])
 
+# Zählen, wie oft Werte aus `left` in `right` vorkommen
+result = {value: right.count(value) for value in left}
 
-# Sortieren der Listena
-left_sort = sorted(left)
-right_sort = sorted(right)
-
-# Löschen von Altlasten
-del left
-del right
-
-# Berechnen der Paarungen
-index = []
-for runner in range(len(left_sort)):
-    value = int(left_sort[runner]) - int(right_sort[runner])
-    # Betragsfunktion
-    if value < 0:
-        value *= -1
-    index.append(value)
-    del value
-
-# Ermittle das Ergebniss
-result = 0
-for runner in index:
-    result += runner
-
-# Test Ausgabe
+# Ausgabe des Ergebnisses
 print(result)
